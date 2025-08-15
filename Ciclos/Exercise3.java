67d6734c3c5762669a0418df44a3601bdb1acbdd
kevinlp9 <128652425+kevinlp9@users.noreply.github.com>
2025-08-15 11:39:52 -0600

    15/08/25

--- Ciclos/Exercise3.java
+++ Ciclos/Exercise3.java
@@ -5,35 +5,36 @@ import java.util.Scanner;
 public class Exercise3 {
     public static void main(String[] args) {
         double notas, notas6=0, notas5=0, notas1=0;
-        double prom1=0, prom2=0, prom3=0, promtotal=0;
+        double promtotal=0;
         Scanner sc = new Scanner(System.in);
-        int j=1, k=1, l=1;
+        //contador para cantidad de calificaciones
+        //sobre la cual vamos a calcular el promedio
+        int j=0, k=0, l=0;
         for (int i=1; i<=20; i++){
             System.out.println("Ingresa la nota: " + i);
             notas=sc.nextDouble();
             promtotal+=notas;
                 if (notas>=6 && notas<=10){
+                    //si la nota cumple las condiciones se agrega a la suma
                     notas6+=notas;
-                    prom1=notas6/j;
+                    //y se incrementa el contador para saber sobre cuanto se debe promediar
                     j++;
                 } else if (notas<6 && notas>1) {
                     notas5+=notas;
-                    prom2=notas5/k;
                     k++;
                 } else if (notas==1) {
                     notas1+=notas;
-                    prom3=notas1/l;
                     l++;
                 } else {
+                    //si no cumplen las condiciones se cierra
                     return;
                 }
             }
         sc.close();
-        System.out.println("La cantidad de notas mayores e iguales a 6 es: " + j + " y el promedio es: " + prom1);
-        System.out.println("La cantidad de notas menores a 6 es: " + k + " y el promedio es: " + prom2 );
-        System.out.println("La cantidad de notas iguales a 1 es: " + l + " y el promedio es: " + prom3);
+        System.out.println("La cantidad de notas mayores e iguales a 6 es: " + j + " y el promedio es: " + notas6/j);
+        System.out.println("La cantidad de notas menores a 6 es: " + k + " y el promedio es: " + notas5/k );
+        System.out.println("La cantidad de notas iguales a 1 es: " + l + " y el promedio es: " + notas1/l);
         System.out.println("El promedio total es: " + promtotal/20);
         }
-
     }
 
